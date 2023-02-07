e3feaa1a9db1cdda3904f3821d7afb454ca75bd0
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-02-07 15:39:15 +0200

    Add files

---
+++ src/js/api-service.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const BASE_URL = 'https://pokeapi.co/api/v2';
+
+// function fetchPokemon(pokemonId) {
+//   return fetch(`${BASE_URL}/pokemon/${pokemonId}`).then(response =>
+//     response.json(),
+//   );
+// }
+
+// export default { fetchPokemon };
---
+++ src/js/get-refs.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function getRefs() {
+//   return {
+//     cardContainer: document.querySelector('.js-card-container'),
+//     searchForm: document.querySelector('.js-search-form'),
+//   };
+// }
